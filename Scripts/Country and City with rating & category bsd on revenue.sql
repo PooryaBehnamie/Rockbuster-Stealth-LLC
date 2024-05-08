49bf01c1fe7d912db0d57361eab68185007a7993
Poorya Behnamie <169101583+PooryaBehnamie@users.noreply.github.com>
2024-05-08 13:05:40 -0400

    Update Country and City with rating & category bsd on revenue.sql

--- Scripts/Country and City with rating & category bsd on revenue.sql
+++ Scripts/Country and City with rating & category bsd on revenue.sql
@@ -1,9 +1,9 @@
-SELECT  CO.country,
-		CI.city,
-		F.rating,
-		CA.name AS Category,
-		SUM(P.amount) AS Total_revenue,
-		COUNT(R.rental_id) AS Rental_count
+SELECT   CO.country,
+	 CI.city,
+	 F.rating,
+	 CA.name AS Category,
+	 SUM(P.amount) AS Total_revenue,
+	 COUNT(R.rental_id) AS Rental_count
 FROM film F
 JOIN language L ON F.language_id = L.language_id
 JOIN film_category FM ON F.film_id = FM.film_id
@@ -15,10 +15,10 @@ JOIN customer C ON P.customer_id = C.customer_id
 JOIN address AD ON C.address_id = AD.address_id
 JOIN city CI ON AD.city_id = CI.city_id
 JOIN country CO ON CI.country_id = CO.country_id
-GROUP BY CO.country,
-		 CI.city,
-		 F.rating,
-		 CA.name
-ORDER BY Total_revenue DESC, 
-		 Rental_count DESC
+GROUP BY  CO.country,
+	  CI.city,
+	  F.rating,
+	  CA.name
+ORDER BY  Total_revenue DESC, 
+	  Rental_count DESC
  
